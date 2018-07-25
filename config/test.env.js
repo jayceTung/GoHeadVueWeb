7cc50cfc6e38d8cfe7bfc868d932ff8ae49895b1
Super <yinte112@163.com>
2018-07-25 14:06:41 +0800

    Initial commit

---
+++ config/test.env.js
@@ -0,0 +1,9 @@
+'use strict'
+const merge = require('webpack-merge')
+const devEnv = require('./dev.env')
+
+module.exports = merge(devEnv, {
+  NODE_ENV: '"testing"',
+  BASE_API: '"https://api-sit"',
+  APP_ORIGIN: '"https://wz.com"'
+})
---
+++ src/main.js
@@ -0,0 +1,48 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue';
+import App from './App';
+import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
+import axios from 'axios';
+import ElementUI from 'element-ui';
+import Vuex from 'vuex';
+import HelloWorld from './components/HelloWorld.vue';
+import Head from './components/Head.vue';
+
+import './common/stylus/index.styl';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.config.productionTip = false
+
+Vue.use(VueRouter);
+Vue.use(ElementUI);
+Vue.use(VueResource);
+Vue.use(Vuex);
+Vue.config.debug = true;
+
+Vue.prototype.$ajax = axios
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/head',
+    name: '',
+    hidden: true
+  }, {
+    path: '/head',
+    component: Head,
+    name: '',
+    hidden: true
+  }
+];
+const router = new VueRouter({
+  routes,
+  linkActiveClass: 'active'
+});
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+});
