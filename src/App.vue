785606c9804f9f9d990660d22aeae8aa5596e6e1
Super <yinte112@163.com>
2018-07-31 11:21:55 +0800

    modify:整理

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,11 @@
 <template>
   <div id="app">
-    <!--<div class="tab-item">-->
-      <!--<router-link to="/goods">商品</router-link>-->
-    <!--</div>-->
-    <!--<div class="tab-item">-->
-      <!--<router-link to="/ratings">评论</router-link>-->
-    <!--</div>-->
-    <!--<div class="tab-item">-->
-      <!--<router-link to="/seller">商家</router-link>-->
-    <!--</div>-->
-    <router-view :seller="seller"></router-view>
+    <router-view></router-view>
   </div>
 </template>
 
-<script type="text/ecmascript-6">
-  const ERR_OK = 0;
+<script>
   export default {
-    data() {
-      return {
-        seller: {}
-      };
-    },
-    created() {
-      this.$http.get('/api/seller').then((response) => {
-        response = response.body;
-        if (response.errno === ERR_OK) {
-          this.seller = response.data;
-          console.log(this.seller);
-        }
-      });
-    }
-  };
+    name: 'app'
+  }
 </script>
-
-<style lang="stylus" rel="stylesheet/stylus">
-  @import "./common/stylus/mixin.styl"
-  #app
-    .tab
-      display: flex
-      width: 100%
-      height:40px
-      line-height: 40px
-      border-1px(rgba(7, 17, 27, 0.1))
-      .tab-item
-        flex:1
-        text-align:center
-        & > a
-          display:block
-          color:rgb(77,85,93)
-          font-size:14px
-        & .active
-          color:#f01414
-</style>
--- src/main.js
+++ src/main.js
@@ -1,54 +1,32 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue';
-import App from './App';
-import VueRouter from 'vue-router';
-import VueResource from 'vue-resource';
-import axios from 'axios';
-import ElementUI from 'element-ui';
-import Vuex from 'vuex';
-import HelloWorld from './components/HelloWorld.vue';
-import Head from './components/Head.vue';
-import Seller from './components/food/Seller.vue';
+import Vue from 'vue'
 
-import './common/stylus/index.styl';
-import 'element-ui/lib/theme-chalk/index.css';
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+// import locale from 'element-ui/lib/locale/lang/en'
+import 'fullcalendar/dist/fullcalendar.min.css'
+import './styles/index.scss' // global css
+import App from './App'
+import router from './router'
+import store from './store'
+import '@/icons' // icon
+import '@/permission' // permission control
+
+import FullCalendar from 'vue-full-calendar'
+
+Vue.use(FullCalendar)
+
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
-Vue.use(VueRouter);
-Vue.use(ElementUI);
-Vue.use(VueResource);
-Vue.use(Vuex);
-Vue.config.debug = true;
-
-Vue.prototype.$ajax = axios
-
-const routes = [
-  {
-    path: '/',
-    redirect: '/head',
-    name: '',
-    hidden: true
-  }, {
-    path: '/head',
-    component: Head,
-    name: 'Head',
-    hidden: true
-  }, {
-    path: '/seller',
-    component: Seller,
-    name: 'Seller',
-    hidden: true
-  }
-];
-const router = new VueRouter({
-  routes,
-  linkActiveClass: 'active'
-});
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
-});
+})
